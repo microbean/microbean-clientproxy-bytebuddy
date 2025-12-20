5183d506f95a8dec0623bf64c4f537b27bf81b23
microbean <ci@microbean.org>
2025-12-20 08:01:34 +0000

    Site checkin for project microBean™ Client Proxy: Byte Buddy

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.microbean.clientproxy.bytebuddy","l":"org.microbean.clientproxy.bytebuddy"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"m":"org.microbean.clientproxy.bytebuddy","l":"org.microbean.clientproxy.bytebuddy"}];updateSearchResults();
